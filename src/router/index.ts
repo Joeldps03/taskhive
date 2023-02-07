85e8aa90f8823851b4c34334a7ea670988cb18f7
Your Name <joeldepalol@gmail.com>
2023-02-07 19:10:50 +0100

    dimarts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,59 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
-    children: [
-      {
-        path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
-      },
-    ],
+    path: "",
+    name: "portada",
+    component: () => import("@/views/portada.vue"),
   },
-]
+  {
+    path: "/iniciarSessio",
+    name: "iniciarSessio",
+
+    component: () => import("@/views/iniciarSessio.vue"),
+  },
+  {
+    path: "/tasques",
+    name: "tasques",
+
+    component: () => import("@/views/tasques.vue"),
+  },
+  {
+    path: "/tasques/editar",
+    name: "editarTasques",
+
+    component: () => import("@/views/editarTasques.vue"),
+  },
+  {
+    path: "/crearTasques",
+    name: "crearTasques",
+
+    component: () => import("@/views/crearTasques.vue"),
+  },
+  {
+    path: "/crearUsuaris",
+    name: "crearUsuaris",
+
+    component: () => import("@/views/crearUsuaris.vue"),
+  },
+  {
+    path: "/usuairs/editar",
+    name: "editarUsuaris",
+
+    component: () => import("@/views/editarUsuaris.vue"),
+  },
+  {
+    path: "/usuaris",
+    name: "usuaris",
+
+    component: () => import("@/views/usuaris.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+export default router;
---
+++ src/views/tasques.vue
@@ -0,0 +1,10 @@
+<template>
+hola
+  
+</template>
+
+<script lang="ts">
+  export default {
+  name: "tasques",
+}
+</script>
