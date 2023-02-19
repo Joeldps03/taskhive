f24ad0e718c1126a8c2f0033ff51ae10eec7ce7f
Arnau Soler <a2106901@institutmontilivi.cat>
2023-02-19 20:08:12 +0100

    api comentada

--- api/api.php
+++ api/api.php
@@ -9,18 +9,16 @@ class Server
         $data=json_decode(file_get_contents('php://input'));
         $token= null;
 
-        //mirem si lki estem passant un token
+        //mirem si li estem passant un token
         if($data!= null){
             $token=$data->token;
-            // if ($data->id != null){
-            //     $id=$data->id;
-            // }
         }
         $uri = $_SERVER['REQUEST_URI'];
         $method = $_SERVER['REQUEST_METHOD'];
         $paths = explode('/', $uri);
         array_shift($paths); // Hack; get rid of initials empty string
         array_shift($paths);
+        //agafem l'accio que volem desenvolupar
         $accio = array_shift($paths);
         $resource = array_shift($paths);
 
@@ -29,50 +27,40 @@ class Server
            echo(json_encode($bdd->inserirtoken_tokens()));
         }
         else if($token!= null){
+            //mirem si el token és nostre
            if($bdd->existeixtokentokens($token) || $bdd->existeixtokenusuarisbool($token)){
                 if ($accio == 'login') {
                     //Mirem si existeix el usuari
-                    // if ($bdd->login($data->correu,$data->contrasenya)) {
-
                         $token1=$bdd->login($data->correu,$data->contrasenya);
                         $pasar=array("token"=>$token1);
                         if($token1)
                             echo json_encode($pasar);
-                    // } else
-                    //     header('HTTP/1.1 404 usuari no trobat');
                 }
+                //acció que la utilitzem per a editar tasques
                 else if ($accio == 'llistarunatasca') {
-                    //Controlem el tipus d'usuari
                     $llistartasca=null;
                     $llistartasca=$bdd->llistarunatasca($data->id);
                     $pasar=array("tasca"=>$llistartasca);
                     echo json_encode($pasar);
                 }
+                //aquí llistem les tasques
                 else if ($accio == 'tasques') {
-                    //Controlem el tipus d'usuari
                     $usuari=$bdd->existeixtokenusuaris($token);
                     $llistartasques=null;
+                    //mirem rol per saber les tàsques que hem de llistar
                     if ($usuari["rol"] == "tecnic") {
                         $llistartasques=$bdd->llistarTasquesUser($usuari["id"]);
                     } else {
                         $llistartasques=$bdd->llistarTasques();
                         header('HTTP/1.1 202 Goel Gestor');
                     }
+                    //retornem les tasques llistades a l'usuari
                     $pasar=array("usuari"=>$usuari,"tasques"=>$llistartasques);
                     echo json_encode($pasar);
                 }
                 else if ($accio == 'editartasques') {
-                    // if ($method == "POST") {
-                    //     //Aquí hem de controlar el rol del usuari que fa la petició
-                    //     if ($data->correu == "tecnic") {
-                    //         $bdd->editartasquestecnic($data->id,$data->estat, $data->comentaris_tecnics);
-                    //     } else {
-                            //Tant el gestor com l'admin poden editar la tasca sencera
-                        $bdd->editartasques($data->id,$data->nom, $data->descripcio, $data->id_usuari, $data->prioritat, $data->estat, $data->comentaris_tecnics);
-                        // }
-                    // } else {
-                    //     header('HTTP/1.1 405 Mètode no disponible');
-                    // }
+                    //cridem a la funcio de la api per a editar les tasques
+                    $bdd->editartasques($data->id,$data->nom, $data->descripcio, $data->id_usuari, $data->prioritat, $data->estat, $data->comentaris_tecnics);
                 }
                 else if ($accio == 'creartasca') {
                     if ($method == "POST") {
@@ -82,19 +70,9 @@ class Server
                     }
                 }
                 else if ($accio == 'eliminar') {
-                    if ($method == "POST") {
-                        $bdd->eliminartasques($data->id);
-                    } else {
-                        header('HTTP/1.1 405 Mètode no disponible');
-                    }
+                    $bdd->eliminartasques($data->idtasca);
                 }
-                else if ($accio == 'editarusuari') {
-                    if ($method == "POST") {
-                        $bdd->editarusuari($_POST["id"], $_POST["nom"], $_POST["email"], $_POST["rol"], $_POST["contrasenya"]);
-                    } else {
-                        header('HTTP/1.1 405 Mètode no disponible');
-                    }
-                }
+                //funció per a llistar els usuaris
                 else if ($accio == 'usuari') {
                     $llistarusuaris=null;
                     $llistarusuaris=$bdd->llistarusuaris();
@@ -107,7 +85,9 @@ class Server
                     } else {
                         header('HTTP/1.1 405 Mètode no disponible');
                     }
-                } else {
+                }
+                //si no revem cap acció li enviem aquest header 
+                else {
                     header('HTTP/1.1 404 metode no trobat');
                 }
             }
