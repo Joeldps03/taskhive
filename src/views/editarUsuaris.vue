8d5e2d44ec9ef2625954c0820fff2d9d71de37ad
Your Name <joeldepalol@gmail.com>
2023-02-19 17:45:10 +0100

    editarUsuarisVue

--- src/views/editarUsuaris.vue
+++ src/views/editarUsuaris.vue
@@ -12,7 +12,7 @@
             <h3>Nom</h3>
 
             <v-col cols="6">
-              <v-text-field clearable :rules="rules"></v-text-field>
+              <v-text-field  v-model="nom" clearable :rules="rules" ></v-text-field>
             </v-col>
           </div>
 
@@ -20,7 +20,7 @@
           <h3>Correu</h3>
 
           <v-col cols="10">
-            <v-text-field clearable :rules="rules"></v-text-field>
+            <v-text-field v-model="correu" clearable :rules="rules" ></v-text-field>
           </v-col>
           </div>
 
@@ -28,7 +28,7 @@
           <h3>Contrasenya</h3>
 
           <v-col cols="10">
-            <v-text-field clearable :rules="rules"></v-text-field>
+            <v-text-field v-model="contrasenya" clearable :rules="rules" ></v-text-field>
           </v-col>
           </div>
 
@@ -36,7 +36,7 @@
             <h3>Rol</h3>
 
             <v-col cols="6">
-              <v-text-field clearable :rules="rules"></v-text-field>
+              <v-text-field v-model="rol" clearable :rules="rules" ></v-text-field>
             </v-col>
           </div>
 
@@ -47,6 +47,8 @@
               size="large"
               type="submit"
               variant="outlined"
+              @click="editarUsuari()"
+              
             >
               Editar
             </v-btn>
@@ -58,13 +60,47 @@
 </template>
 <script>
 import layout from "@/layouts/default/layout.vue";
+import axios from 'axios';
 
 export default {
   components: { layout },
-  name: "editarUsuaris",
-  prop: ["info"]
+  name: "editarusuari",
+  data() {
+    return {
+      nom: '',
+      correu: '',
+      contrasenya: '',
+      rol: '',
+      loading: false,
+      rules: [
+        v => !!v || 'Camp requerit'
+      ]
+    };
+  },
+  methods: {
+    editarUsuari() {
+      axios.post("http://localhost/api/editarusuari/", {
+      token: sessionStorage.tokenusuari,
+      nom: this.nom,
+      email: this.correu,
+      contrasenya: this.contrasenya,
+      rol: this.rol,
+  })
+    .then(response => {
+      console.log(response)
+      this.$router.push("/usuaris");
+    })
+    .catch(error => {
+      console.error(error); // manejar el error de la solicitud
+    });
+},
+  },
 };
 </script>
+
+<style src="@/styles/settings.scss">
+
+</style>
 
 <style src="@/styles/settings.scss">
 v-table {
